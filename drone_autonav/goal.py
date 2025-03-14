d8c33bd3fe47e01c5bab1350bccf4773fc731748
Jagadeesh-pradhani <01fe21bee114@kletech.ac.in>
2025-03-15 01:39:37 +0530

    goal loop debug

--- drone_autonav/goal.py
+++ drone_autonav/goal.py
@@ -4,6 +4,7 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Point, PoseStamped
 from std_msgs.msg import Empty
+from nav_msgs.msg import Odometry
 
 class DroneGoalPublisher(Node):
     def __init__(self):
@@ -13,22 +14,33 @@ class DroneGoalPublisher(Node):
         # Subscriber to know when a goal has been reached
         self.create_subscription(Empty, '/goal_reached', self.goal_reached_callback, 10)
 
-        # Define the six goals as (x, y, z)
+        self.subscription_odom = self.create_subscription(Odometry, '/odom', self.odom_callback, 10)
+        self.ugv_position = None
+        # Define the five goals as (x, y, z)
         self.goals = [
             (-4.0, 4.0, 3.0),
             (-1.0, 4.0, 3.0),
             (-1.0, 0.0, 3.0),
             (-4.0, 0.0, 3.0),
             (-4.0, -4.0, 3.0),
-            (1.0, 0.0, 3.0)
+            # (1.0, 0.0, 3.0)
         ]
         self.current_goal_index = 0
 
         # Publish the first goal immediately
         self.publish_goal()
 
+    def odom_callback(self, msg : Odometry):
+        """Callback for odometry messages."""
+
+        self.ugv_position = msg.pose.pose.position
+        self.ugv_orientation = msg.pose.pose.orientation
+        # print(self.ugv_position)
+
     def publish_goal(self):
         """Publish the current goal to /drone_goal."""
+
+        
         if self.current_goal_index < len(self.goals):
             goal = self.goals[self.current_goal_index]
             point_msg = PoseStamped()
@@ -38,7 +50,14 @@ class DroneGoalPublisher(Node):
                 f'Published goal {self.current_goal_index+1}: (x: {point_msg.pose.position.x}, y: {point_msg.pose.position.y}, z: {point_msg.pose.position.z})'
             )
         else:
+            point_msg = PoseStamped()
+            point_msg.pose.position.x, point_msg.pose.position.y, point_msg.pose.position.z = self.ugv_position.x, self.ugv_position.y, 3.0
+            self.goal_publisher.publish(point_msg)
+            self.get_logger().info(
+                f'Published goal {self.current_goal_index+1}: (x: {point_msg.pose.position.x}, y: {point_msg.pose.position.y}, z: {point_msg.pose.position.z})'
+            )
             self.get_logger().info('Mission complete. All goals have been published.')
+            exit()
 
     def goal_reached_callback(self, msg):
         """Callback when a goal is reached. Move to the next goal."""
@@ -48,6 +67,7 @@ class DroneGoalPublisher(Node):
             self.publish_goal()
         else:
             self.get_logger().info('All goals reached. Mission completed.')
+            self.publish_goal()
 
 def main(args=None):
     rclpy.init(args=args)
